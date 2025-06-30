99fe17716e2170cd17ba0f077fe60b411b7da78b
pawanarya9121 <pawanarya560@gmail.com>
2025-06-30 17:34:44 +0530

    first commit

---
+++ Core/DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+   public static void main(String[] args) {
+      int num = 1 ;
+      char ch = 'c' ;
+      String str = "string" ;
+      float ft = 2.0f ;
+      double db = 22.22 ;
+      boolean bl = true ;
+
+      System.out.println(num);
+      System.out.println(ch);
+      System.out.println(str);
+      System.out.println(ft);
+      System.out.println(db);
+      System.out.println(bl);
+   }
+}
---
+++ Core/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+   public static void main(String[]args){
+      int num1 = 10 ;
+      int num2 = 20 ;
+      int sum ;
+      sum = num1+num2 ;
+      System.out.println(sum);
+   }
+}
